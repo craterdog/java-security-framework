2312f02ec8c405789968401ac1c9f2ef8dd2617a
web-online <web-online@users.noreply.github.com>
2015-05-17 00:13:49 -0600

    try and clean the code up a bit and fix the help option

--- java-digital-notary-providers/src/main/java/craterdog/security/DigitalNotaryMain.java
+++ java-digital-notary-providers/src/main/java/craterdog/security/DigitalNotaryMain.java
@@ -14,10 +14,10 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
-import org.apache.commons.cli.BasicParser;
-import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.MissingArgumentException;
+import org.apache.commons.cli.MissingOptionException;
+import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.io.FileUtils;
@@ -28,33 +28,30 @@ import org.apache.commons.io.FileUtils;
  *
  * @author web-online
  */
-public class DigitalNotaryMain {
+public class DigitalNotaryMain extends DefaultParser {
 
+    /**
+     * Notarization implementation
+     */
     private static final Notarization notarization = new RsaDigitalNotary();
 
-    private static final String DEFAULT_OUT_FILE = "notary-key.json";
-
-    private static final String CMD_LINE_SYNTAX = "generate-notary-key [-pubfile <pem file> -prvfile <pem file>]";
-
     public static void main(String[] args) throws ParseException, IOException {
-        HelpFormatter help = new HelpFormatter();
-        Options options = new Options();
-        options.addOption("help", false, "print this message");
-        options.addOption("pubfile", true, "public key input file");
-        options.addOption("prvfile", true, "private key input file");
+        String script_name = "generate-notary-key";
+        DigitalNotaryMain main = new DigitalNotaryMain();
+        Option pubfileOption = Option.builder("pubfile").hasArg().argName("filename").desc("pem encoded public key input file").build();
+        Option prvfileOption = Option.builder("prvfile").hasArg().argName("filename").desc("pem encoded PKCS#8 format encrypted private key input file").build();
 
         try {
-            CommandLine cli = new BasicParser().parse(options, args);
-            String pubfile = cli.getOptionValue("pubfile");
-            String prvfile = cli.getOptionValue("prvfile");
-
+            main.parse(args, pubfileOption, prvfileOption);
             NotaryKey notaryKey = notarization.generateNotaryKey();
 
+            String pubfile = main.cmd.getOptionValue(pubfileOption.getOpt());
+            String prvfile = main.cmd.getOptionValue(prvfileOption.getOpt());
             if (pubfile != null || prvfile != null) {
                 if (pubfile == null)
-                    throw new MissingArgumentException("Missing option: pubfile");
+                    throw new MissingOptionException("Missing option: pubfile");
                 if (prvfile == null)
-                    throw new MissingArgumentException("Missing option: prvfile");
+                    throw new MissingOptionException("Missing option: prvfile");
 
                 CertificateManager manager = new RsaCertificateManager();
                 PublicKey publicKey = manager.decodePublicKey(FileUtils.readFileToString(new File(pubfile)));
@@ -69,12 +66,60 @@ public class DigitalNotaryMain {
                 notarization.documentIsValid("test document", seal, publicKey);
             }
             char[] password = System.console().readPassword("verficationKey password: ");
-            FileUtils.writeStringToFile(new File(DEFAULT_OUT_FILE), notarization.serializeNotaryKey(notaryKey, password));
-        } catch (MissingArgumentException | FileNotFoundException ex) {
+            writeObjectToFile("notary-key.json", notarization.serializeNotaryKey(notaryKey, password));
+        } catch (ParseException | FileNotFoundException ex) {
             System.out.println(ex.getMessage());
-            help.printHelp(CMD_LINE_SYNTAX, options);
+            main.printHelp(script_name);
             System.exit(1);
+        } catch (ClassCastException ex) {
+            String message = ex.getMessage();
+            if (message != null && message.contains("PEMEncryptedKeyPair cannot be cast to org.bouncycastle.pkcs.PKCS8EncryptedPrivateKeyInfo")) {
+                System.out.println("Incorrect format for prvfile. Expected PKCS#8 format encrypted private key.");
+                main.printHelp(script_name);
+                System.exit(1);
+            }
         }
+    }
+
+    /**
+     * Utility method for writing an Object out to a file.
+     * @param fileName the name of the file to write
+     * @param data the data to be written by calling toString
+     * @throws IOException 
+     */
+    public static void writeObjectToFile(String fileName, Object data) throws IOException {
+        FileUtils.writeStringToFile(new File(fileName), data.toString());
+        System.out.println(fileName + " has been created");
+    }
+
+    /**
+     * Parse the command line arguments with the given additionalOptions.
+     * @param arguments command line arguments
+     * @param additionalOptions options to add for parsing/validation
+     * @throws ParseException 
+     */
+    public void parse(String[] arguments, Option... additionalOptions) throws ParseException {
+        Option helpOption = new Option("help", false, "print this message");
+        options = new Options().addOption(helpOption);
+        for (Option option : additionalOptions) {
+            options.addOption(option);
+        }
+        try {
+            parse(options, arguments, null);
+        } finally {
+            // help hides any other exceptions
+            if (cmd.hasOption(helpOption.getOpt())) {
+                throw new ParseException(helpOption.getOpt());
+            }
+        }
+    }
+
+    /**
+     * Print a help message with a usage
+     * @param script_name 
+     */
+    public void printHelp(String script_name) {
+        new HelpFormatter().printHelp(script_name, options, true);
     }
 
 }
